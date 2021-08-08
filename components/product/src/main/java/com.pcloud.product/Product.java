4e1f6f60018f48147858171e6b1a4eec472bd708
PCloud63514 <pcloud63514@gmail.com>
2021-08-08 13:58:56 +0900

    Product 설정 중

--- components/product/src/main/java/com.pcloud.product/Product.java
+++ components/product/src/main/java/com.pcloud.product/Product.java
@@ -1,23 +1,40 @@
 package com.pcloud.product;
 
+import javassist.NotFoundException;
 import lombok.Builder;
 import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="dtype")
 @Getter
 @Setter
 public class Product {
     @Id @Generated
+    @Column(name="product_id")
     private Long id;
     private String name;
-    private String description;
+    private int price;
+    private int stockQuantity;
 
-    @Builder
-    public Product(String name, String description) {
-        this.name = name;
-        this.description = description;
+    @ManyToMany(mappedBy="products", fetch = FetchType.LAZY)
+    private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int resultStock = this.stockQuantity - quantity;
+        if (0 > resultStock) {
+            throw new NotEnoughStockException("need nore stock");
+        }
+        this.stockQuantity = resultStock;
     }
 }
